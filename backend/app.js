b245f0084d2a1b53d691ee7784ce52c34d248508
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-21 21:03:25 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors"
+
+const allowedOrigins =  [
+    "http://localhost:5173",
+    // "https://incrediblesaizan1-code-review.vercel.app", 
+    ];
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          console.log("Blocked origin:", origin);
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+    );  
+app.options("*", cors());
+ 
+
+app.get("/", (req, res)=>{
+    res.send("hello")
+})
+
+app.listen( process.env.PORT  || 2000)
