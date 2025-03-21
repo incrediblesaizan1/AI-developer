6153900d7d49584ccda0c6465c9dfb363ffa27f7
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-03-21 16:11:10 +0530

    s

--- backend/app.js
+++ backend/app.js
@@ -189,13 +189,14 @@ app.post("/login", async (req, res) => {
       "lslsdlsdlsfndnvlsklskdssldsldsl"
     );
 
-    return res
-      .status(200)
-      .json({
-        Error: false,
-        message: "You Logged In Successfully",
-        user: { email: user.email, accessToken },
-      });
+    // return res
+    //   .status(200)
+    //   .json({
+    //     Error: false,
+    //     message: "You Logged In Successfully",
+    //     user: { email: user.email, accessToken },
+    //   });
+    res.send(user)
   } catch (error) {
     console.log("Something went wrong while login user", error);
     res.status(500).end("Something went wrong while login user");
